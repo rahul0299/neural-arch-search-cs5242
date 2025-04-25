727b5a4f24fb87be141ad25254c6564e394f45b0
Rahul Bhargava <rahulbhargava1999@gmail.com>
2025-04-25 10:20:15 +0800

    exp 6 results [500 iters]

--- reward_functions.py
+++ reward_functions.py
@@ -1,3 +1,6 @@
+import math
+import torch
+
 from CNN_NAS.ChildCNNModel import ChildCNNModel
 
 
@@ -5,4 +8,15 @@ def test_accuracy_reward(model: ChildCNNModel, test_data, **kwargs):
     test_x, test_y = test_data
     accuracy, validation_loss = model.evaluate_model(test_x, test_y, **kwargs)
 
-    return (accuracy, validation_loss, accuracy) # accuracy, reward value
+    return accuracy, validation_loss, accuracy  # accuracy, reward value
+
+
+
+def test_accuracy_with_model_complexity_reward(model, test_data, w1=1, w2=0.05, **kwargs):
+    test_x, test_y = test_data
+    accuracy, validation_loss = model.evaluate_model(test_x, test_y, **kwargs)
+
+    model_complexity = sum(p.numel() for p in model.parameters())
+    reward = w1 * accuracy - w2 * math.log(model_complexity)
+
+    return accuracy, validation_loss, reward
