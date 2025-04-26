9bbf2d56894817ab385e447cf2d6d2d4c64c3729
Ezio99 <vaibhavritam@gmail.com>
2025-04-26 12:10:29 +0800

    Removed unnecessary functions

--- utils.py
+++ utils.py
@@ -450,50 +450,6 @@ def save_progress(controller, optimizer, baseline, row_data, controller_type="CN
 def load_progress(name, controller_type="CNN"):
     return pd.read_csv(os.path.join("Results", "Controller", controller_type, f"{name}.csv"))
 
-
-def plot_accuracy(name, controller_type="CNN"):
-    df = load_progress(name, controller_type)
-    plt.plot(df["train_acc"])
-
-def plot_metrics(name, controller_type="CNN", metrics=None, labels=None, titles=None, colors=None,limit=None):
-    df = load_progress(name, controller_type)
-
-    if limit is not None:
-        df = df.head(limit)
-
-    if metrics is None:
-        metrics = df.columns
-    else:
-        for m in metrics:
-            if m not in df.columns:
-                raise ValueError(f"Metric '{m}' not found in progress data.")
-
-    if labels is None:
-        labels = { m: m for m in metrics }
-
-    if titles is None:
-        titles = { m: m.replace("_", " ").capitalize() for m in metrics }
-
-    if colors is None:
-        colors = {m: get_random_color_hex_code() for m in metrics}
-
-    fig, axs = plt.subplots(len(metrics), 1, figsize=(10, 4 * len(metrics)))
-
-    # fig, axs = plt.subplots(3, 1, figsize=(10, 12))
-    fig.suptitle(f"{controller_type} Controller Training Progress ({name})", fontsize=16)
-
-    for i, metric in enumerate(metrics):
-        axs[i].plot(df[metric], label=labels[metric], color=colors[metric] if metric in colors else get_random_color_hex_code())
-        axs[i].set_title(titles[metric])
-        axs[i].set_xlabel("Iteration")
-        axs[i].set_ylabel(metric)
-        axs[i].grid(True)
-        axs[i].legend()
-
-    plt.tight_layout(rect=[0, 0, 1, 0.96])
-    plt.show()
-
-
 def reset_experiment(name, controller_type="CNN"):
     model_file_path = os.path.join(os.path.join("Models", "Controller", controller_type, f"{name}.pt"))
 
@@ -539,7 +495,3 @@ def save_child_model(child_model, accuracy, name, controller_type="CNN"):
         'model_state_dict': child_model.state_dict(),
         'accuracy': accuracy
     }, path)
-
-
-def get_random_color_hex_code():
-    return "#{:06x}".format(random.randint(0, 0xFFFFFF))
